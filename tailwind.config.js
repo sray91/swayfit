e3cccb0b4a16ce1c4252e69f44bc37b3e3a8c29d
swanagan <swanaganray@gmail.com>
2022-02-21 22:50:24 -0500

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
-    colors: { 
-      'orange': '#ffa32c',
-      'purple': '#550069'
+    colors: {
+      "orange":"#ffa32c",
+      "purple": "#550069"
     }
   },
   plugins: [],
