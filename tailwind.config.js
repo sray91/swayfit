d226f55599219b9c52047a3a4693d903d55962bd
swanagan <swanaganray@gmail.com>
2022-02-22 19:05:07 -0500

    nothing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
     colors: {
-      "orange":"#ffa32c",
+      "orange": "#ffa32c",
       "purple": "#550069",
       "white": "#ffffff"
     }
