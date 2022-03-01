5078e4586f6d3c5af13543a6734c806014cb7c09
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2022-03-01 11:13:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
               <Image src="/swayfit_logo.svg" alt="SwayFit Logo" width={653} height={400} />
           </span>
           <button onClick={ () => connectWallet("injected")}
-            className="h-11 border-0 w-auto px-40 rounded-md cursor-pointer text-base font-bold text-white bg-gradient-to-r from-violet-500 to-fuchsia-500">
+            className="h-11 border-0 w-auto px-40 rounded-md cursor-pointer text-base font-bold text-white bg-purple">
             Connect Wallet
           </button>
         </main>
