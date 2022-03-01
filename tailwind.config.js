70366ded966b10526f85749b7cf77a160547b181
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2022-03-01 11:19:50 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,7 @@
 module.exports = {
   content: ["./pages/*.{html,js}"],
   theme: {
-    extend: {
-     'animation': {
-            'gradient-x':'gradient-x 15s ease infinite',
-            'gradient-y':'gradient-y 15s ease infinite',
-            'gradient-xy':'gradient-xy 15s ease infinite',
-        },
-      'keyframes': {
-            'gradient-y': {
-                '0%, 100%': {
-                    'background-size':'400% 400%',
-                    'background-position': 'center top'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'center center'
-                }
-            },
-            'gradient-x': {
-                '0%, 100%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            },
-            'gradient-xy': {
-                '0%, 100%': {
-                    'background-size':'400% 400%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            }
-        }
-    },
+    extend: {},
     colors: {
       "orange": "#ffa32c",
       "purple": "#550069",
